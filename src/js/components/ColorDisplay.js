866bc3f3424499e9273eafb083cabcae01cd076a
Thiago Kaique Ribeiro Fonseca <66787043+Thiago099@users.noreply.github.com>
2021-03-19 17:32:52 -0300

    Update ColorDisplay.js

--- src/js/components/ColorDisplay.js
+++ src/js/components/ColorDisplay.js
@@ -1,23 +1,33 @@
 import React from 'react'
 import './ColorDisplay.css';
 
-export default e=>
+export default e =>
 {
-    function componentToHex(c) {
-        var hex = parseInt(c).toString(16);
-        return hex.length == 1 ? "0" + hex : hex;
-      }
+    
     return <>
       <div class="color" style={{backgroundColor:`rgba(${e.color.r},${e.color.g},${e.color.b},1)`}}></div>    
       <div className="displayContainer">
         <label htmlFor="">Hex:</label>
-        <input type="text" className="display" name="" id="" value={"#" + componentToHex(e.color.r) + componentToHex(e.color.g) + componentToHex(e.color.b)}/>
+        <input type="text" className={`display ${e.hex.length==7?'':'invalid'}`} name="" id="" value={e.hex} onChange={f=>{
+          e.setHex(f.target.value.substr(0,7));
+           var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(f.target.value);
+           if(result)
+            e.update("hex",parseInt(result[1], 16),
+              parseInt(result[2], 16),
+             parseInt(result[3], 16));
+        }}/>
         <label htmlFor="">R:</label>
-        <input type="text" className="display" name="" id="" value={parseInt(e.color.r)}/>
+        <input type="text" className="display" name="" id="" value={e.color.r} onChange={f=>{
+            e.update('input',f.target.value>255?255:f.target.value<0?0:f.target.value,e.color.g,e.color.b)
+          }}/>
         <label htmlFor="">G:</label>
-        <input type="text" className="display" name="" id="" value={parseInt(e.color.g)}/>
+        <input type="text" className="display" name="" id="" value={e.color.g} onChange={f=>{
+            e.update('input',e.color.r,f.target.value>255?255:f.target.value<0?0:f.target.value,e.color.b)
+          }}/>
         <label htmlFor="">B:</label>
-        <input type="text" className="display" name="" id="" value={parseInt(e.color.b)}/>
+        <input type="text" className="display" name="" id="" value={e.color.b} onChange={f=>{
+            e.update('input',e.color.r,e.color.g,f.target.value>255?255:f.target.value<0?0:f.target.value)
+          }}/>
       </div>
 
 
